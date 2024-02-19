b2dad616d1a24351c6615e17476b1085cde356a2
Vinicius Ferreira Bezerra <vinicius.ferreira.bezerra07@aluno.ifce.edu.br>
2024-02-19 20:00:49 -0300

    Adição da url da imagem do pokemon

--- src/Components/CardPokemon/CardPokemons.tsx
+++ src/Components/CardPokemon/CardPokemons.tsx
@@ -5,13 +5,13 @@ import './CardPokemon.css'
 
 export default function CardPokemon({ name, type, img }) {
 
-    console.log(img.official_artwork);
+    console.log(img["official-artwork"]?.front_default);
     
     return (
         <Link to={'/pokemon${}'}>
             <div className="cardPokemon" >
                 <Icon type={type[0].type.name} />
-                <img className="pokeImg" src={img} />
+                <img className="pokeImg" src={img["official-artwork"]?.front_default} />
                 <h1 className="pokeName">{name}</h1>
             </div>
         </Link>
